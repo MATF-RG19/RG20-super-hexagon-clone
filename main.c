fdb6e91af30789c760a1e74443ae944fe3145afe
Aleksa Tesic <tesic@getpenta.com>
2019-11-30 22:15:46 +0100

    Add first version of the timed animation

--- main.c
+++ main.c
@@ -3,16 +3,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+#define TIMER_ID (0)
+#define TIMER_INTERVAL (20)
+
 #define KEY_ESCAPE (27)
 #define KEY_LEFT ('j')
 #define KEY_RIGHT ('l')
+
+#define HEXAGON_X (0.87)
+#define HEXAGON_Y (0.5)
+#define HEXAGON_X_AXIS (0.0)
+#define HEXAGON_Y_AXIS (1.0)
+
+#define HEXAGON_SCALING_FACTOR (0.95)
 
 
 static void on_keyboard(unsigned char key, int x, int y);
 static void on_display(void);
 static void on_reshape(int width, int height);
+static void on_timer(int value);
+
 
 static int window_width, window_height;
+static int animation_ongoing;
+
+static double curr_x = HEXAGON_X;
+static double curr_y = HEXAGON_Y;
+static double curr_x_axis = HEXAGON_X_AXIS;
+static double curr_y_axis = HEXAGON_Y_AXIS;
 
 int main(int argc, char** argv)
 {
@@ -23,10 +42,11 @@ int main(int argc, char** argv)
     glutInitWindowPosition(100, 100);
     glutCreateWindow(argv[0]);
 
-
     glutKeyboardFunc(on_keyboard);
     glutReshapeFunc(on_reshape);
     glutDisplayFunc(on_display);
+
+    animation_ongoing = 0;
 
     glClearColor(0.75, 0.75, 0.75, 0.0);
     glEnable(GL_DEPTH_TEST);
@@ -43,6 +63,10 @@ void on_keyboard(unsigned char key, int x, int y)
         break;
     case KEY_LEFT:
         printf("go left\n");
+        if(!animation_ongoing) {
+            glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
+            animation_ongoing = 1;
+        }
         break;
     
     case KEY_RIGHT:
@@ -57,23 +81,54 @@ void on_reshape(int width, int height)
     window_width = width;
 }
 
+
+static void on_timer(int value)
+{
+    if (value != TIMER_ID)
+        return;
+
+    glLoadIdentity();
+    glRotatef(5.0, 0, 0, 1);
+    glScalef(0.9, 0.9, 0.9);
+
+    curr_x *= HEXAGON_SCALING_FACTOR;
+    curr_y *= HEXAGON_SCALING_FACTOR;
+
+    curr_x_axis *= HEXAGON_SCALING_FACTOR;
+    curr_y_axis *= HEXAGON_SCALING_FACTOR;
+    
+    printf("x_curr = %lf\ny_curr=%lf\n", curr_x, curr_y);
+    printf("x_curr_axis = %lf\ny_curr_axis=%lf\n", curr_x_axis, curr_y_axis);
+
+
+    glutPostRedisplay();
+
+    if (animation_ongoing) {
+        glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
+    }
+}
 void on_display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     
-    double x = 0.87; // ~ sqrt(3) / 2
-    double y = 0.5;
+    // double x = 0.87; // ~sqrt(3) / 2
+    // double y = 0.5;
 
     glColor3f(0, 0, 1);
+    glLoadIdentity();
+    
+    //! FIXME
+    // glRotatef(20.0, 0, 0, 1);
+    // glScalef(0.9, 0.9, 0.9);
+    
     glBegin(GL_LINE_LOOP);
-        glVertex3f(0, 1, 0);
-        glVertex3f(x, y, 0);
-        glVertex3f(x, -y, 0);
-        glVertex3f(0, -1, 0);
-        glVertex3f(-x, -y, 0);
-        glVertex3f(-x, y, 0);
-
+        glVertex3f(curr_x_axis,  curr_y_axis,   0);
+        glVertex3f(curr_x,       curr_y,        0);
+        glVertex3f(curr_x,      -curr_y,        0);
+        glVertex3f(curr_x_axis, -curr_y_axis,   0);
+        glVertex3f(-curr_x,     -curr_y,        0);
+        glVertex3f(-curr_x,      curr_y,        0);
     glEnd();
 
     glutSwapBuffers();
