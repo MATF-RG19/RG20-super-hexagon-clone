9b8240f57d644b37e3ae8402fe10e97f7a84f6c9
Aleksa Tesic <tesic@getpenta.com>
2019-12-02 15:12:21 +0100

    Add proper scaling and rotation

--- main.c
+++ main.c
@@ -11,12 +11,12 @@
 #define KEY_LEFT ('j')
 #define KEY_RIGHT ('l')
 
-#define HEXAGON_X (0.87)
+#define HEXAGON_X (0.87) // ~sqrt(3) / 2
 #define HEXAGON_Y (0.5)
 #define HEXAGON_X_AXIS (0.0)
 #define HEXAGON_Y_AXIS (1.0)
-
 #define HEXAGON_SCALING_FACTOR (0.95)
+#define HEXAGON_ROTATE_STEP (20)
 
 
 static void on_keyboard(unsigned char key, int x, int y);
@@ -32,6 +32,9 @@ static double curr_x = HEXAGON_X;
 static double curr_y = HEXAGON_Y;
 static double curr_x_axis = HEXAGON_X_AXIS;
 static double curr_y_axis = HEXAGON_Y_AXIS;
+
+static double scaling_factor = 1;
+static int rotation_step = 0;
 
 int main(int argc, char** argv)
 {
@@ -81,25 +84,10 @@ void on_reshape(int width, int height)
     window_width = width;
 }
 
-
 static void on_timer(int value)
 {
     if (value != TIMER_ID)
         return;
-
-    glLoadIdentity();
-    glRotatef(5.0, 0, 0, 1);
-    glScalef(0.9, 0.9, 0.9);
-
-    curr_x *= HEXAGON_SCALING_FACTOR;
-    curr_y *= HEXAGON_SCALING_FACTOR;
-
-    curr_x_axis *= HEXAGON_SCALING_FACTOR;
-    curr_y_axis *= HEXAGON_SCALING_FACTOR;
-    
-    printf("x_curr = %lf\ny_curr=%lf\n", curr_x, curr_y);
-    printf("x_curr_axis = %lf\ny_curr_axis=%lf\n", curr_x_axis, curr_y_axis);
-
 
     glutPostRedisplay();
 
@@ -110,11 +98,7 @@ static void on_timer(int value)
 void on_display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
     
-    // double x = 0.87; // ~sqrt(3) / 2
-    // double y = 0.5;
-
     glColor3f(0, 0, 1);
     glLoadIdentity();
     gluLookAt (
@@ -122,10 +106,10 @@ void on_display(void)
         0.0, 0.0, 0.0, 
         0.0, 2.0, 0.0
     );
-    //! FIXME
-    // glRotatef(20.0, 0, 0, 1);
-    // glScalef(0.9, 0.9, 0.9);
     
+    glRotatef(rotation_step, 0, 0, 1);
+    glScalef(scaling_factor, scaling_factor, scaling_factor);
+
     glBegin(GL_LINE_LOOP);
         glVertex3f(curr_x_axis,  curr_y_axis,   0);
         glVertex3f(curr_x,       curr_y,        0);
@@ -134,6 +118,15 @@ void on_display(void)
         glVertex3f(-curr_x,     -curr_y,        0);
         glVertex3f(-curr_x,      curr_y,        0);
     glEnd();
+
+    rotation_step += HEXAGON_ROTATE_STEP;
+    scaling_factor *= HEXAGON_SCALING_FACTOR;
+
+    printf("x_curr = %lf\ny_curr=%lf\n", curr_x, curr_y);
+    printf("x_curr_axis = %lf\ny_curr_axis=%lf\n", curr_x_axis, curr_y_axis);
+
+
+    
 
     glutSwapBuffers();
 }
