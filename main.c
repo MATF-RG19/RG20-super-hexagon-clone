d1010e8a5eea383a95bcbddd018e3dc9392ae752
Aleksa Tesic <tesic@getpenta.com>
2019-12-10 23:23:52 +0100

    Convert to lines, add buggy random egde removal

--- main.c
+++ main.c
@@ -27,6 +27,8 @@
 #define HEXAGON_NEGATIVE_ROTATION_DIRECTION (-1)
 #define HEXAGON_STARTING_SCALE_FACTOR (2)
 
+#define ILLEGAL_EDGE (-1)
+
 #define EPSILON (0.3)
 
 #define NUMBER_OF_HEXAGONS (4)
@@ -50,7 +52,7 @@ static double rotation_step = 0;
 static double rotation_direction = HEXAGON_POSITIVE_ROTATION_DIRECTION;
 
 static double hexagon_edge_length[NUMBER_OF_HEXAGONS] = {2.0, 1.5, 1.0, 0.50};
-static int already_has_removed_edge[NUMBER_OF_HEXAGONS] = {0, 0, 0, 0};
+static int current_removed_edge[NUMBER_OF_HEXAGONS] = {2, 3, 1, ILLEGAL_EDGE};
 
 int main(int argc, char** argv)
 {
@@ -154,9 +156,6 @@ static void on_display(void)
     rotation_step += HEXAGON_ROTATION_STEP;
     updateScalingFactors();
     
-
-    // printf("rotation_direction: %lf\n", rotation_direction);
-
     glutSwapBuffers();
 }
 
@@ -166,7 +165,7 @@ static void drawHexagon(int hexagon_idx)
 
     glPushMatrix();
         glScalef(scale_factor, scale_factor, scale_factor); 
-        glBegin(GL_LINE_LOOP);
+        glBegin(GL_LINES);
             draw_partial_hexagon();
         glEnd();
     glPopMatrix();
@@ -176,23 +175,46 @@ static void draw_partial_hexagon()
 {
     int idx_to_be_removed = rand() % 6;
     printf("idx to be removed: %d\n", idx_to_be_removed);
-    int ver_num = 6;
-    GLfloat vertices[6][3] = {
+    int ver_num = 12;
+    GLfloat vertices[12][3] = {
+        // 0
         {HEXAGON_X_AXIS,  HEXAGON_Y_AXIS,           0},
         {HEXAGON_X,       HEXAGON_Y,               0},
+
+        // 1
+        {HEXAGON_X,       HEXAGON_Y,               0},
+        {HEXAGON_X,       -HEXAGON_Y,               0},
+        
+        // 2
         {HEXAGON_X,       -HEXAGON_Y,               0},
         {HEXAGON_X_AXIS,  -HEXAGON_Y_AXIS,          0},
+        
+        // 3
+        {HEXAGON_X_AXIS,  -HEXAGON_Y_AXIS,          0},
+        {-HEXAGON_X,      -HEXAGON_Y,               0},
+        
+        // 4
         {-HEXAGON_X,      -HEXAGON_Y,               0},
         {-HEXAGON_X,      HEXAGON_Y,                0},
+
+        // 5
+        {-HEXAGON_X,      HEXAGON_Y,                0},
+        {HEXAGON_X_AXIS,  HEXAGON_Y_AXIS,           0}
     };
 
     for(int i = 0; i < ver_num; i++) {
-        glVertex3fv(vertices[i]);
-        // if(idx_to_be_removed != i && !already_has_removed_edge[i]) {
-        //     glVertex3fv(vertices[i]);
-        //     already_has_removed_edge[i] = 1;
-        // }
+
+        // FIXME DOESNT WORK, IT NEEDS TO SAVE THE REMOVED EDGE AND TO REMOVE IT AGAIN IN THE NEXT ITERATION
+        if(i % 2 == 0 && current_removed_edge[i/2] == ILLEGAL_EDGE) {
+            current_removed_edge[i/2] = idx_to_be_removed;
+        }
     }
+    for(int i = 0; i < ver_num; i++) {
+        if(current_removed_edge[i/2] != i) {
+            glVertex3fv(vertices[i]);
+        }
+    }
+
 }
 
 static void updateScalingFactors() 
@@ -200,7 +222,7 @@ static void updateScalingFactors()
     for (int i = 0; i < NUMBER_OF_HEXAGONS; i++) {
         if(hexagon_edge_length[i] < EPSILON) {
             hexagon_edge_length[i] = get_randomized_scaling_factor();
-            already_has_removed_edge[i] = 0;
+            //current_removed_edge[i] = 0;
         }
         else {
             hexagon_edge_length[i] *= HEXAGON_SCALING_FACTOR;
