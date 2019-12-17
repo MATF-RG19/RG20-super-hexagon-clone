a1b3bc80d4b136eb77d2a96234e22c8337e34349
Aleksa Tesic <tesic@getpenta.com>
2019-12-18 00:40:08 +0100

    Ädd TODO for improved edge detection

--- main.c
+++ main.c
@@ -24,10 +24,10 @@
 #define HEXAGON_Y_AXIS (1.0)
 #define HEXAGON_SCALING_FACTOR (0.995)
 #define HEXAGON_ROTATION_STEP (1)
+// TODO: deterimine if the edge is non-existent based on the hexagon rotation step, in order to have passable terrain we need to have angle between -10 and 10
 #define HEXAGON_POSITIVE_ROTATION_DIRECTION (1)
 #define HEXAGON_NEGATIVE_ROTATION_DIRECTION (-1)
 #define HEXAGON_STARTING_SCALE_FACTOR (2)
-
 
 #define LOWER_LIMIT (20)
 #define UPPER_LIMIT (35)
@@ -37,7 +37,6 @@
 #define ILLEGAL_VALUE (-1)
 #define MIN_DISTANCE (0.5)
 #define SAFE_DISTANCE (0.015)
-
 #define EPSILON (0.095)
 
 #define NUMBER_OF_HEXAGONS (4)
@@ -57,7 +56,9 @@ static float getRandomizedScalingFactor();
 static void drawPartialHexagon();
 static void drawAllHexagons();
 static void rearrangeHexagons();
+
 static void checkForImpassableTerrain(); 
+
 static void drawAgent();
 
 static float calculateDistance(int idx0, int idx1);
