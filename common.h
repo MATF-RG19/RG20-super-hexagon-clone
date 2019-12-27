72fa480554ec6bbadb5333fd91cf72d56a5ff923
Aleksa Tesic <tesic@getpenta.com>
2019-12-27 18:03:50 +0100

    Moved macros to common.h

---
+++ common.h
@@ -0,0 +1,46 @@
+#define WINDOW_WIDTH (1920)
+#define WINDOW_HEIGHT (1000)
+#define WINDOW_POS_X (1080 / 2)
+#define WINDOW_POS_Y (1920 / 2)
+
+#define CAMERA_FOV (45)
+
+#define TIMER_ID (0)
+#define TIMER_INTERVAL (17) //? ~60fps
+
+#define KEY_ESCAPE (27)
+#define KEY_LEFT ('a')
+#define KEY_RIGHT ('d')
+#define KEY_STOP ('s')
+#define KEY_PAUSE_ROTATION (' ')
+#define KEY_CHANGE_SHADE_MODEL ('m')
+
+#define HEXAGON_X (0.87) //? ~sqrt(3) / 2
+#define HEXAGON_Z (0.5)
+#define HEXAGON_X_AXIS (0.0)
+#define HEXAGON_Z_AXIS (1.0)
+#define HEXAGON_SCALING_FACTOR (0.996)
+#define HEXAGON_ROTATION_STEP (1)
+#define HEXAGON_POSITIVE_ROTATION_DIRECTION (1)
+#define HEXAGON_NEGATIVE_ROTATION_DIRECTION (-1)
+#define HEXAGON_STARTING_SCALE_FACTOR (2)
+#define LOWER_LIMIT (120)
+#define UPPER_LIMIT (190)
+
+#define NO_DISTANCE (-1)
+#define ILLEGAL_VALUE (-1)
+
+#define MIN_DISTANCE_BETWEEN_HEXAGONS (4)
+#define COLLISION_SAFE_DISTANCE (0.010)
+#define EPSILON (0.095)
+
+#define NUMBER_OF_HEXAGONS (5)
+
+#define TEXT_POS_X (1.895)
+#define TEXT_POS_Z (1.225)
+#define TEXT_VERTICAL_OFFSET (0.075)
+
+#define TEXTURE_AGENT "img/yellow.bmp"
+#define TEXTURE_GAME_OVER "img/game_over.bmp"
+
+
