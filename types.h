1564c31a3fa494f9db2076006adac17f7cf80972
Aleksa Tesic <tesic@getpenta.com>
2019-12-27 18:09:30 +0100

    Add types.h

---
+++ types.h
@@ -0,0 +1,24 @@
+#ifndef TYPES_H
+#define TYPES_H
+#define GL_SILENCE_DEPRECATION
+#include <GLUT/glut.h>
+
+typedef GLfloat point[3];
+
+typedef struct {
+    point* vertices;
+    int index;
+    float distances[3]; 
+    int removed_edge_index_1;
+    int removed_edge_index_2;
+    float scaling_factor;
+    int left_angle;
+    int right_angle;
+    int removed_edge;
+} Hexagon;
+
+typedef struct {
+    point* agent_pos;
+} Agent;
+
+#endif 
